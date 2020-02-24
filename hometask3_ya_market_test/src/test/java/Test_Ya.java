023ce895ac33799bc08be15425cb1b9df68cd5a7
ТвоÑÐOolgas <>
2020-02-24 23:37:30 +0300

    1.time of waiting was decreased 2.added base methods 3.url announced as class field instead of repeatble in each test

--- hometask3_ya_market_test/src/test/java/Test_Ya.java
+++ hometask3_ya_market_test/src/test/java/Test_Ya.java
@@ -17,67 +17,59 @@ public class Test_Ya {
 
     public static final Logger logger = LogManager.getLogger(Test_Ya.class.getName());
     private WebDriver driver;
-    private String baseUrl;
+    private String baseUrl = "https://market.yandex.ru/catalog--mobilnye-telefony/54726/list";
     WebDriverWait wait;
+    String popupText,titleOfPhone,popupTextTwoDevices;
+
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp()  {
         //WebDriverManager.chromedriver().arch32().proxy(ConfigProxy.httpProxy).setup();
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         logger.info("WebDriver initialization...");
         baseUrl = "https://market.yandex.ru/catalog--mobilnye-telefony/54726/list";
-        wait = new WebDriverWait(driver, 15);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.get("https://www.yandex.ru/");
-    }
-
-    @Test
-    public void addXiaomiToCompare(){
+        wait = new WebDriverWait(driver, 8);
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
         driver.get("https://www.yandex.ru/");
         logger.info("Get yandex.ru");
-        driver.get("https://market.yandex.ru/catalog--mobilnye-telefony/54726/list");
-        logger.info("Get market.yandex.ru");
-        driver.findElement(By.xpath("//span[contains(text(),'Xiaomi')]")).click();
-        driver.findElement(By.xpath("//span[contains(text(),'HUAWEI')]")).click();
-        driver.findElement(By.cssSelector("fieldset[data-autotest-id='16816262'] > ul > li:last-child > div > label")).click();
-        driver.findElement(By.linkText("по цене")).click();
-        logger.info("Filters applied ");
-
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".preloadable__preloader_visibility_visible")));
-        String firstXiaomi = driver.findElement(By.cssSelector(":nth-child(1) > .n-snippet-cell2__header a")).getAttribute("title");
-        driver.findElement(By.xpath("//div[contains(@class, 'n-product-toolbar__item')]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".popup-informer__details")));
-        String popupTextXiaomi = driver.findElement(By.cssSelector(".popup-informer__title")).getText();
-        logger.info("Select of the first Xiaomi smartphone, adding to compare ");
-        Assert.assertEquals(String.format("Товар " + firstXiaomi + " добавлен к сравнению"), popupTextXiaomi);
     }
 
-    @Test
-    public void addHUAVEIToCompare() {
-        //предусловие для выполнение теста - вызов предыдущего метода
-        addXiaomiToCompare();
-        logger.info("Previous test executed ");
+    @Test//Проверить, что отобразилась плашка "Товар {имя товара} добавлен к сравнению
+    public void xiaomiAdd(){
+        driver.get(baseUrl);
+        logger.info("Get market.yandex.ru");
+        sortOfphones();
+        addPhoneToCompare();
+        Assert.assertEquals(String.format("Товар " + titleOfPhone + " добавлен к сравнению"), popupText);
 
-        //избавление от сматфона Xoimi в списке, чтобы первым в списке был гарантировано HUAVEI
+    }
+
+    @Test//Проверить, что отобразилась плашка "Товар {имя товара} добавлен к сравнению/два товара в сравнении
+    public void huaveiAdd(){
+        driver.get(baseUrl);
+        logger.info("Get market.yandex.ru");
+        sortOfphones();
+        addPhoneToCompare();
         driver.findElement(By.xpath("//span[contains(text(),'Xiaomi')]")).click();
-
         wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".preloadable__preloader_visibility_visible")));
-        String firstHUAVEI = driver.findElement(By.cssSelector(":nth-child(1) > .n-snippet-cell2__header a")).getAttribute("title");
-        driver.findElement(By.xpath("//div[contains(@class, 'n-product-toolbar__item')]")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".popup-informer__details")));
-        String popupTextTwoDevices = driver.findElement(By.cssSelector(".popup-informer__details")).getText();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".popup-informer__title")));
-        String popupTextHUAVEI = driver.findElement(By.cssSelector(".popup-informer__title")).getText();
-        logger.info("Select of the first Huavei smartphone, adding to compare ");
-        Assert.assertEquals(String.format("Товар " + firstHUAVEI + " добавлен к сравнению"), popupTextHUAVEI);
+        addPhoneToCompare();
+        Assert.assertEquals(String.format("Товар " + titleOfPhone + " добавлен к сравнению"), popupText);
         Assert.assertEquals(String.format("Всего в списке 2 товара из категории Мобильные телефоны"), popupTextTwoDevices);
         driver.findElement(By.linkText("Сравнить")).click();
+
     }
 
-    @Test
-    public void comparingOfPhones_diffCharacteristics() {
-        driver.get("https://market.yandex.ru/compare/2AcMac7Bg1MAv5ciEUjBYPC71wy9?hid=91491&id=217969159&id=401338434");
+    @Test // Проверить, что позиция "Операционная система" не отображается в списке характеристик
+    public void checkOfDiff(){
+        driver.get(baseUrl);
+        logger.info("Get market.yandex.ru");
+        sortOfphones();
+        addPhoneToCompare();
+        driver.findElement(By.xpath("//span[contains(text(),'Xiaomi')]")).click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".preloadable__preloader_visibility_visible")));
+        addPhoneToCompare();
+        driver.findElement(By.linkText("Сравнить")).click();
         List<WebElement> listOfDiffCompareElements = driver.findElements(By.cssSelector(".n-compare-row-name i-bem"));
         logger.info("List of elements presented in comparing created");
         //перебор созданного списка на предмет совпадения с Операционная система
@@ -89,9 +81,16 @@ public class Test_Ya {
         }
     }
 
-    @Test
-    public void comparingOfPhones_allCharacteristics() {
-        driver.get("https://market.yandex.ru/compare/2AcMac7Bg1MAv5ciEUjBYPC71wy9?hid=91491&id=217969159&id=401338434");
+    @Test // Проверить, что в списке характеристик появилась позиция "Операционная система"
+    public void checkOfAll(){
+        driver.get(baseUrl);
+        logger.info("Get market.yandex.ru");
+        sortOfphones();
+        addPhoneToCompare();
+        driver.findElement(By.xpath("//span[contains(text(),'Xiaomi')]")).click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".preloadable__preloader_visibility_visible")));
+        addPhoneToCompare();
+        driver.findElement(By.linkText("Сравнить")).click();
         driver.findElement(By.xpath("//span[@class='link n-compare-show-controls__all']//span[@class='link__inner']")).click();
         logger.info("All characteristics part opened");
         List<WebElement> listOfCompareElements = driver.findElements(By.cssSelector(".n-compare-row-name i-bem"));
@@ -105,8 +104,27 @@ public class Test_Ya {
         }
     }
 
+    public void sortOfphones() {
+        driver.findElement(By.xpath("//span[contains(text(),'Xiaomi')]")).click();
+        driver.findElement(By.xpath("//span[contains(text(),'HUAWEI')]")).click();
+        driver.findElement(By.cssSelector("fieldset[data-autotest-id='16816262'] > ul > li:last-child > div > label")).click();
+        driver.findElement(By.linkText("по цене")).click();
+        logger.info("Filters applied ");
+    }
+
+    public void addPhoneToCompare(){
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".preloadable__preloader_visibility_visible")));
+        titleOfPhone = driver.findElement(By.cssSelector(":nth-child(1) > .n-snippet-cell2__header a")).getAttribute("title");
+        driver.findElement(By.xpath("//div[contains(@class, 'n-product-toolbar__item')]")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".popup-informer__details")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".popup-informer__title")));
+        popupText = driver.findElement(By.cssSelector(".popup-informer__title")).getText();
+        popupTextTwoDevices = driver.findElement(By.cssSelector(".popup-informer__details")).getText();
+        logger.info("Select of the smartphone, adding to compare ");
+    }
+
     @After
-    public void setDown() throws Exception {
+    public void setDown()  {
         if (driver != null) {
             driver.quit();
         }
